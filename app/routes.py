e7eb02f1d3c11e9f0227003503ee281a0324bac3
martins3939 <147891140+martins3939@users.noreply.github.com>
2024-07-25 22:21:52 +0100

    feat: add html templates

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, Welcome to Storepy!"
+    return render_template('index.html', title='Home')
