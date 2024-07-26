f6ff717ca1eb70a4f3d6fc72be7aaa4f42235b79
martins3939 <147891140+martins3939@users.noreply.github.com>
2024-07-27 00:12:03 +0100

    feat: create database
    Create new database with User table

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'anime-greasily6-justice-bacon-skimming-chloride-repacking-greeter'
+    SECRET_KEY = (os.environ.get('SECRET_KEY') or 'anime-greasily6-justice-bacon-skimming-chloride-repacking-greeter')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
