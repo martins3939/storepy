e57be182e4b3145673fa6b34d388553be77533b0
martins3939 <147891140+martins3939@users.noreply.github.com>
2024-07-28 12:00:12 +0100

    feat: add file table
    Add File table to database

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
+from datetime import datetime
 
 
 class User(db.Model):
@@ -12,3 +13,15 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+
+class Files(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey('user.id'), index=True)
+    file_name: so.Mapped[str] = so.mapped_column(sa.String(120), index=True)
+    file_type: so.Mapped[str] = so.mapped_column(sa.String(120), index=True)
+    file_size: so.Mapped[int] = so.mapped_column(sa.Integer, index=True)
+    upload_date: so.Mapped[str] = so.mapped_column(sa.DateTime, default=datetime.now(), index=True)
+
+    def __repr__(self):
+        return '<Files {}>'.format(self.file_name)
