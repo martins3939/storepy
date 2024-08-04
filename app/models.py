aea7e487ba3ee091afb5bd28c3f2c454c6abf47e
martins3939 <147891140+martins3939@users.noreply.github.com>
2024-08-04 18:20:46 +0100

    fix: fix file table
    Created a new db with fixed model file and a new migrate repo

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
-from datetime import datetime
+from datetime import datetime, timezone
 
 
 class User(db.Model):
@@ -15,13 +15,13 @@ class User(db.Model):
         return '<User {}>'.format(self.username)
 
 
-class Files(db.Model):
+class File(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey('user.id'), index=True)
     file_name: so.Mapped[str] = so.mapped_column(sa.String(120), index=True)
     file_type: so.Mapped[str] = so.mapped_column(sa.String(120), index=True)
     file_size: so.Mapped[int] = so.mapped_column(sa.Integer, index=True)
-    upload_date: so.Mapped[str] = so.mapped_column(sa.DateTime, default=datetime.now(), index=True)
+    upload_date: so.Mapped[datetime] = so.mapped_column(default=lambda: datetime.now(timezone.utc), index=True)
 
     def __repr__(self):
         return '<Files {}>'.format(self.file_name)
