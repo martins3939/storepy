2a97ac7a9dd6d8b6990150cb30f9fb4f51bc8bd6
martins3939 <147891140+martins3939@users.noreply.github.com>
2024-08-06 00:03:38 +0100

    feat: initiate flask-login

--- app/models.py
+++ app/models.py
@@ -1,12 +1,13 @@
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-from app import db
+from app import db, login
 from datetime import datetime, timezone
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
@@ -32,3 +33,8 @@ class File(db.Model):
 
     def __repr__(self):
         return '<Files {}>'.format(self.file_name)
+
+
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
